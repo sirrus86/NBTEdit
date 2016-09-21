b937e2c68c6e39789e61b6aa50a661b779a9226a
sirrus86 <sirrus86@hotmail.com>
2016-09-21 00:31:28 -0400

    Complete some preparatory work in plugin's main class

--- src/main/java/me/sirrus86/nbtedit/NBTEdit.java
+++ src/main/java/me/sirrus86/nbtedit/NBTEdit.java
@@ -2,6 +2,19 @@ package me.sirrus86.nbtedit;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.sirrus86.nbtedit.nms.NMSLib;
+
 public class NBTEdit extends JavaPlugin {
 
+	private NMSLib nms;
+	
+	@Override
+	public void onEnable() {
+		nms = null; //TODO
+	}
+	
+	public NMSLib getNMSLib() {
+		return nms;
+	}
+	
 }
